aec2b3ad6a3f2344c3775139d45513c63344bfc9
Lamy210 <syoytaku@gmail.com>
2024-01-08 06:48:18 +0900

    ADD.devcontainer

---
+++ src_file/src/VITS/module/dataset_util.py
@@ -0,0 +1,155 @@
+#encoding:utf-8
+
+import random
+import numpy as np
+import matplotlib as mpl
+mpl.use('Agg')# AGG(Anti-Grain Geometry engine)
+import matplotlib.pyplot as plt
+import os
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+from torchvision import models,transforms
+import torchvision.utils as vutils
+import torch.nn.init as init
+from torch.autograd import Function
+import torch.nn.functional as F
+
+import torchaudio
+
+#wavファイル、話者id、テキスト(音素列)の3つを読み込むためのDatasetクラス
+class AudioSpeakerTextLoader(torch.utils.data.Dataset):
+	"""
+		1) 前処理によって作成されたtxtファイルに書かれたwavファイル、話者id、テキスト(音素列)の3つを読み込む
+		2) テキストを正規化し整数へと変換
+		3) wavファイルからスペクトログラムを計算
+	"""
+	def __init__(self, dataset_txtfile_path, phoneme_list):
+		#dataset_txtfile_path : 前処理によって作成されたtxtファイルへのパス
+		#phoneme_list : 学習に用いる音素のlist
+		self.sampling_rate = 22050
+		self.filter_length = 1024
+		self.hop_length = 256
+		self.win_length = 1024
+		self.phoneme_list = phoneme_list
+		#音素とindexを対応付け　対応を前計算しておくことでバッチ作成時の処理を高速化する
+		self.phoneme2index = {p : i for i, p in enumerate(self.phoneme_list, 0)}
+
+		###前処理によって作成されたtxtファイルの読み込み###
+		#一行につき
+		#wavファイルへのパス|話者id|音素列
+		#というフォーマットで記述されている
+		with open(dataset_txtfile_path, "r") as f:
+			self.wavfilepath_speakerid_text = [line.split("|") for line in f.readlines()]
+		#各行をランダムにシャッフル
+		random.seed(1234)
+		random.shuffle(self.wavfilepath_speakerid_text)
+
+	def get_audio_text_speaker_pair(self, audiopath_sid_text):
+		# separate filename, speaker_id and text
+		audiopath, sid, text = audiopath_sid_text[0], audiopath_sid_text[1], audiopath_sid_text[2]
+		wav, spec = self.get_audio(audiopath)
+		text = self.get_text(text)
+		sid = self.get_sid(sid)
+		return (wav, spec, text, sid)
+
+	def get_audio(self, wavfile_path):
+		#wavファイルの読み込み
+		wav, _ = torchaudio.load(wavfile_path)
+		#wavからspectrogramを計算
+		pad_size = int((self.filter_length-self.hop_length)/2)
+		wav_padded = torch.nn.functional.pad(wav, (pad_size, pad_size), mode='reflect')
+		spec = torchaudio.functional.spectrogram(
+								waveform=wav_padded,
+								pad=0,#torchaudio.functional.spectrogram内で使われているtorch.nn.functional.padはmode='constant'となっているが、今回はmode='reflect'としたいため手動でpaddingする
+								window=torch.hann_window(self.win_length),
+								n_fft=self.filter_length,
+								hop_length=self.hop_length,
+								win_length=self.win_length,
+								power=2,
+								normalized=False,
+								center=False
+							)
+		spec = torch.squeeze(spec, 0)
+		return wav, spec
+
+	def get_sid(self, sid):
+		sid = torch.LongTensor([int(sid)])
+		return sid
+	
+	def get_text(self, text):
+		#Converts a string of text to a sequence of IDs corresponding to the symbols in the text.
+		text_splitted = text.replace("\n", "").split(",")
+		text_converted_into_index = [self.phoneme2index[p] for p in text_splitted]#音素を数値に変換
+		#各音素の間に0を挿入する
+		text_norm = [0] * (len(text_converted_into_index) * 2 + 1)
+		text_norm[1::2] = text_converted_into_index
+		#tensorへと変換
+		text_norm = torch.LongTensor(text_norm)
+		return text_norm
+
+	def __getitem__(self, index):
+		line = self.wavfilepath_speakerid_text[index]
+		wavfilepath, speakerid, text = line[0], line[1], line[2]
+		wav, spec = self.get_audio(wavfilepath)
+		speaker_id = self.get_sid(speakerid)
+		text = self.get_text(text)
+		return (wav, spec, speaker_id, text)
+
+	def __len__(self):
+		return len(self.wavfilepath_speakerid_text)
+
+#AudioSpeakerTextLoaderの__getitem__により取得されたデータをバッチへと固める関数
+def collate_fn(batch):
+	# batch = [
+	# 	(wav, spec, speaker_id, text),
+	# 	(wav, spec, speaker_id, text),
+	# 	....
+	# ]
+	max_wav_len = max([x[0].size(1) for x in batch])#wavの最大の長さを算出
+	max_spec_len = max([x[1].size(1) for x in batch])#spectrogramの最大の長さを算出
+	max_text_len = max([x[3].size(0) for x in batch])#textの最大の長さを算出
+
+	batch_size = len(batch)
+
+	wav_lengths = torch.LongTensor(batch_size)#torch.size([batch_size])
+	spec_lengths = torch.LongTensor(batch_size)
+	speaker_id = torch.LongTensor(batch_size)
+	text_lengths = torch.LongTensor(batch_size)
+
+	wav_padded = torch.zeros(batch_size, 1, max_wav_len, dtype=torch.float32)
+	spec_padded = torch.zeros(batch_size, batch[0][1].size(0), max_spec_len, dtype=torch.float32)
+	text_padded = torch.zeros(batch_size, max_text_len, dtype=torch.long)
+
+	#text_padded, spec_padded, wav_paddedは全ての要素が0で初期化されているが、
+	#左詰めで元のtext, spec, wavで上書きすることによりzero-paddingされたtensorを取得できる
+	for i, (wav_row, spec_row, speaker_id_row, text_row) in enumerate(batch, 0):
+		wav_padded[i, :, :wav_row.size(1)] = wav_row
+		wav_lengths[i] = wav_row.size(1)
+
+		spec_padded[i, :, :spec_row.size(1)] = spec_row
+		spec_lengths[i] = spec_row.size(1)
+
+		speaker_id[i] = speaker_id_row
+
+		text_padded[i, :text_row.size(0)] = text_row
+		text_lengths[i] = text_row.size(0)
+
+	return  wav_padded, wav_lengths, \
+			spec_padded, spec_lengths, \
+			speaker_id, \
+			text_padded, text_lengths
+
+#batch内の各tensorについて、start_indices[i]で指定されたindexから長さsegment_sizeの箇所を取り出す関数
+#学習時、スペクトログラムや音声波形について、時間軸に沿って指定した長さだけ切り取るのに用いる
+def slice_segments(input_tensor, start_indices, segment_size):
+	output_tensor = torch.zeros_like(input_tensor[:, ..., :segment_size])
+	batch_size = input_tensor.size(0)
+	for batch_index in range(batch_size):
+		index_start = start_indices[batch_index]
+		index_end = index_start + segment_size
+		output_tensor[batch_index] = input_tensor[batch_index, ..., index_start:index_end]
+	return output_tensor
---
+++ src_file/src/VITS/module/vits_generator.py
@@ -0,0 +1,197 @@
+#encoding:utf-8
+
+import random
+import numpy as np
+import math
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.utils.data as data
+import torchvision
+from torchvision import models,transforms
+from torch.autograd import Function
+import torch.nn.functional as F
+
+#学習用モデルを構成するための各部品
+from .model_component import monotonic_align
+from .model_component.decoder import Decoder
+from .model_component.flow import Flow
+from .model_component.posterior_encoder import PosteriorEncoder
+from .model_component.stochastic_duration_predictor import StochasticDurationPredictor
+from .model_component.text_encoder import TextEncoder
+
+def slice_segments(x, ids_str, segment_size):
+    ret = torch.zeros_like(x[:, :, :segment_size])
+    for i in range(x.size(0)):
+        idx_str = ids_str[i]
+        idx_end = idx_str + segment_size
+        ret[i] = x[i, :, idx_str:idx_end]
+    return ret
+
+def rand_slice_segments(x, x_lengths, segment_size):
+    b, d, t = x.size()
+    ids_str_max = x_lengths - segment_size + 1
+    ids_str = (torch.rand([b]).to(device=x.device) * ids_str_max).to(dtype=torch.long)
+    ret = slice_segments(x, ids_str, segment_size)
+    return ret, ids_str
+
+def sequence_mask(length, max_length=None):
+    if max_length is None:
+        max_length = length.max()
+    x = torch.arange(max_length, dtype=length.dtype, device=length.device)
+    return x.unsqueeze(0) < length.unsqueeze(1)
+
+def generate_path(duration, mask):
+    """
+    duration: [b, 1, t_x]
+    mask: [b, 1, t_y, t_x]
+    """
+    device = duration.device
+    
+    b, _, t_y, t_x = mask.shape
+    cum_duration = torch.cumsum(duration, -1)
+    
+    cum_duration_flat = cum_duration.view(b * t_x)
+    path = sequence_mask(cum_duration_flat, t_y).to(mask.dtype)
+    path = path.view(b, t_x, t_y)
+
+    padding_shape = [[0, 0], [1, 0], [0, 0]]
+    padding = [item for sublist in padding_shape[::-1] for item in sublist]
+
+    path = path - F.pad(path, padding)[:, :-1]
+    path = path.unsqueeze(1).transpose(2,3) * mask
+    return path
+
+#モデルの学習を行うためのクラス
+class VitsGenerator(nn.Module):
+  def __init__(self, n_phoneme, n_speakers):
+    super().__init__()
+    self.n_phoneme = n_phoneme#入力する音素の種類数
+    self.phoneme_embedding_dim = 192#各音素の埋め込み先のベクトルの大きさ
+    self.spec_channels = 513#入力する線形スペクトログラムの縦軸(周波数)の次元
+    self.z_channels = 192#PosteriorEncoderから出力されるzのchannel数
+    self.text_encoders_dropout_during_train = 0.1#学習時のtext_encoderのdropoutの割合
+    self.segment_size = 32#decoderによる音声の生成時、潜在変数zから何要素切り出してdecodeするか
+    self.n_speakers = n_speakers#話者の種類数
+    self.speaker_id_embedding_dim = 256#話者idの埋め込み先のベクトルの大きさ
+
+    #transformerに似た構造のモジュールを用い、音素の列をencodeするネットワーク
+    self.text_encoder = TextEncoder(
+                      n_phoneme=self.n_phoneme,#音素の種類数
+                      phoneme_embedding_dim=self.phoneme_embedding_dim,#各音素の埋め込み先のベクトルの大きさ
+                      out_channels=self.z_channels,#出力するmとlogsのchannel数
+                      p_dropout=self.text_encoders_dropout_during_train#学習時のdropoutの割合
+                    )
+
+    #話者埋め込み用ネットワーク
+    self.speaker_embedding = nn.Embedding(
+                      num_embeddings=self.n_speakers,#話者の種類数
+                      embedding_dim=self.speaker_id_embedding_dim#話者idの埋め込み先のベクトルの大きさ
+                    )
+
+    #linear spectrogramと埋め込み済み話者idを入力にとりEncodeを実行、zを出力するモデル
+    self.posterior_encoder = PosteriorEncoder(
+                      speaker_id_embedding_dim=self.speaker_id_embedding_dim,#話者idの埋め込み先のベクトルの大きさ
+                      in_spec_channels = self.spec_channels,#入力する線形スペクトログラムの縦軸(周波数)の次元
+                      out_z_channels = self.z_channels,#PosteriorEncoderから出力されるzのchannel数
+                      phoneme_embedding_dim = self.phoneme_embedding_dim,#TextEncoderで作成した、埋め込み済み音素のベクトルの大きさ
+                    )
+
+    #z, speaker_id_embeddedを入力にとり音声を生成するネットワーク
+    self.decoder = Decoder(
+                      speaker_id_embedding_dim=self.speaker_id_embedding_dim,#話者idの埋め込み先のベクトルの大きさ
+                      in_z_channel=self.z_channels#入力するzのchannel数
+                    )
+    
+    #Flowとは入出力が可逆なネットワーク
+    #zと埋め込み済み話者idを入力にとり、Monotonic Alignment Searchで用いる変数z_pを出力するネットワーク　逆も可能
+    #音声変換時は、話者間の変換を実行する役割を果たす
+    self.flow = Flow(
+                      speaker_id_embedding_dim=self.speaker_id_embedding_dim,#話者idの埋め込み先のベクトルの大きさ
+                      in_z_channels=self.z_channels,#入力するzのchannel数
+                      phoneme_embedding_dim=self.phoneme_embedding_dim,#TextEncoderで作成した、埋め込み済み音素のベクトルの大きさ
+                    )
+
+    #Text-to-Speechの推論時にはAlignmentを自前で作る必要があるため、StochasticDurationPredictorを用いて音素列の情報から音素継続長を予測する必要がある。
+    #音声変換の推論時には用いない
+    self.stochastic_duration_predictor = StochasticDurationPredictor(
+                      speaker_id_embedding_dim=self.speaker_id_embedding_dim,#話者idの埋め込み先のベクトルの大きさ
+                      phoneme_embedding_dim=self.phoneme_embedding_dim,#TextEncoderで作成した、埋め込み済み音素のベクトルの大きさ
+                      filter_channels=192,
+                      kernel_size=3,
+                      p_dropout=0.5,
+                      n_flows=4
+                    )
+                    
+  def forward(self, text_padded, text_lengths, spec_padded, spec_lengths, speaker_id):
+    #text(音素)の内容をTextEncoderに通す
+    text_encoded, m_p, logs_p, text_mask = self.text_encoder(text_padded, text_lengths)
+
+    #話者idを埋め込み
+    speaker_id_embedded = self.speaker_embedding(speaker_id).unsqueeze(-1)
+    #linear spectrogramと埋め込み済み話者idを入力にとりEncodeを実行、zを出力する
+    z, m_q, logs_q, spec_mask = self.posterior_encoder(spec_padded, spec_lengths, speaker_id_embedded)
+    #zと埋め込み済み話者idを入力にとり、Monotonic Alignment Searchで用いる変数z_pを出力する
+    z_p = self.flow(z, spec_mask, speaker_id_embedded=speaker_id_embedded)
+
+    #Monotonic Alignment Search(MAS)の実行　音素の情報と音声の情報を関連付ける役割を果たす
+    #MASによって、尤度を最大にするようなpathを求める
+    with torch.no_grad():
+        #DPで用いる、各ノードの尤度を前計算しておく
+        s_p_sq_r = torch.exp(-2 * logs_p)
+        neg_cent1 = torch.sum(-0.5 * math.log(2 * math.pi) - logs_p, [1], keepdim=True)
+        neg_cent2 = torch.matmul(-0.5 * (z_p ** 2).transpose(1, 2), s_p_sq_r)
+        neg_cent3 = torch.matmul(z_p.transpose(1, 2), (m_p * s_p_sq_r))
+        neg_cent4 = torch.sum(-0.5 * (m_p ** 2) * s_p_sq_r, [1], keepdim=True)
+        neg_cent = neg_cent1 + neg_cent2 + neg_cent3 + neg_cent4
+        #不要なノードにマスクをかけた上でDPを実行
+        MAS_node_mask = torch.unsqueeze(text_mask, 2) * torch.unsqueeze(spec_mask, -1)
+        MAS_path = monotonic_align.maximum_path(neg_cent, MAS_node_mask.squeeze(1)).unsqueeze(1).detach()
+
+    #text(音素)の各要素ごとに、音素長を計算(各音素長は整数)
+    duration_of_each_phoneme = MAS_path.sum(2)
+    #StochasticDurationPredictorを、音素列の情報から音素継続長を予測できるよう学習させる
+    stochastic_duration_predictor_loss = self.stochastic_duration_predictor(text_encoded, text_mask, duration_of_each_phoneme, speaker_id_embedded=speaker_id_embedded)
+    stochastic_duration_predictor_loss = stochastic_duration_predictor_loss / torch.sum(text_mask)
+
+    m_p = torch.matmul(MAS_path.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2)
+    logs_p = torch.matmul(MAS_path.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2)
+
+    #zの要素からランダムにself.segment_size個取り出しz_sliceとする
+    z_slice, ids_slice = rand_slice_segments(z, spec_lengths, self.segment_size)
+    #z_sliceから音声波形を生成
+    wav_fake = self.decoder(z_slice, speaker_id_embedded=speaker_id_embedded)
+
+    return wav_fake, stochastic_duration_predictor_loss, MAS_path, ids_slice, text_mask, spec_mask, (z, z_p, m_p, logs_p, m_q, logs_q)
+
+  def text_to_speech(self, text_padded, text_lengths, speaker_id, noise_scale=.667, length_scale=1, noise_scale_w=0.8, max_len=None):
+    text_encoded, m_p, logs_p, text_mask = self.text_encoder(text_padded, text_lengths)
+    speaker_id_embedded = self.speaker_embedding(speaker_id).unsqueeze(-1) #話者埋め込み用ネットワーク
+
+    logw = self.stochastic_duration_predictor(text_encoded, text_mask, speaker_id_embedded=speaker_id_embedded, reverse=True, noise_scale=noise_scale_w)
+
+    w = torch.exp(logw) * text_mask * length_scale
+    w_ceil = torch.ceil(w)
+    y_lengths = torch.clamp_min(torch.sum(w_ceil, [1, 2]), 1).long()
+    spec_mask = torch.unsqueeze(sequence_mask(y_lengths, None), 1).to(text_mask.dtype)
+    MAS_node_mask = torch.unsqueeze(text_mask, 2) * torch.unsqueeze(spec_mask, -1)
+    MAS_path = generate_path(w_ceil, MAS_node_mask)
+
+    m_p = torch.matmul(MAS_path.squeeze(1), m_p.transpose(1, 2)).transpose(1, 2)
+    logs_p = torch.matmul(MAS_path.squeeze(1), logs_p.transpose(1, 2)).transpose(1, 2)
+
+    z_p = m_p + torch.randn_like(m_p) * torch.exp(logs_p) * noise_scale
+    z = self.flow(z_p, spec_mask, speaker_id_embedded=speaker_id_embedded, reverse=True)
+    wav_fake = self.decoder((z * spec_mask)[:,:,:max_len], speaker_id_embedded=speaker_id_embedded)
+    return wav_fake
+
+  def voice_conversion(self, spec_padded, spec_lengths, source_speaker_id, target_speaker_id):
+    assert self.n_speakers > 0
+    emb_source = self.speaker_embedding(source_speaker_id).unsqueeze(-1) #話者埋め込み用ネットワーク
+    emb_target = self.speaker_embedding(target_speaker_id).unsqueeze(-1) #話者埋め込み用ネットワーク
+    z, m_q, logs_q, spec_mask = self.posterior_encoder(spec_padded, spec_lengths, speaker_id_embedded=emb_source)
+    z_p = self.flow(z, spec_mask, speaker_id_embedded=emb_source)
+    z_hat = self.flow(z_p, spec_mask, speaker_id_embedded=emb_target, reverse=True)
+    wav_fake = self.decoder(z_hat * spec_mask, speaker_id_embedded=emb_target)
+    return wav_fake
