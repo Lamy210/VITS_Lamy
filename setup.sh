997ecdfecfa06090f740bf881636c49437a64e29
Lamy210 <90091196+Lamy210@users.noreply.github.com>
2024-01-13 01:58:00 +0900

    Delete setup.sh

--- setup.sh
+++
@@ -1,1 +0,0 @@
-apt-get install -y build-essential libasound2-dev && pip install Cython librosa matplotlib numba numpy Pillow scikit-learn scipy six SoundFile torch torchvision torchaudio pyopenjtalk --no-binary pyopenjtalk
